1168eb14e60b39fbacd84d07c4119d8b54adce1a
artic-kuee <76202269+artic-kuee@users.noreply.github.com>
2022-01-07 14:00:34 +0000

    edit schema.sql

--- schema.sql
+++ schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int NOT NULL,
+  `status` int NOT NULL,
+  `host` bigint NOT NULL,
+  `count` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE `members` (
+  `room_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `difficulty` int NOT NULL,
+  `score` bigint DEFAULT NULL,
+  `judge0` bigint DEFAULT NULL,
+  `judge1` bigint DEFAULT NULL,
+  `judge2` bigint DEFAULT NULL,
+  `judge3` bigint DEFAULT NULL,
+  `judge4` bigint DEFAULT NULL,
+  PRIMARY KEY (`room_id`,`user_id`)
+)
